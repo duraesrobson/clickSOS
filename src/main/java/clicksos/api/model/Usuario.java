c97188eda6356d27b957d782cf02b5d22d8b0ce7
Robson Durães <duraesrobson@gmail.com>
2025-09-10 02:48:32 -0300

    feat: add data de nascimento para o usuario

--- src/main/java/clicksos/api/model/Usuario.java
+++ src/main/java/clicksos/api/model/Usuario.java
@@ -1,10 +1,14 @@
 package clicksos.api.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -32,6 +36,10 @@ public class Usuario {
     private Long id;
 
     private String nome;
+
+    @Column(name = "data_nascimento")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
     private String usuario;
     private String senha;
     private String email;
