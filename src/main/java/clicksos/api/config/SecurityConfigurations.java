ea5132b69aac0f026a63ac80c139a41d5e241091
Robson Durães <duraesrobson@gmail.com>
2025-09-10 03:36:15 -0300

    feat: configuracao inicial de seguranca

--- src/main/java/clicksos/api/config/SecurityConfigurations.java
+++ src/main/java/clicksos/api/config/SecurityConfigurations.java
@@ -2,11 +2,23 @@ package clicksos.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/usuarios/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
