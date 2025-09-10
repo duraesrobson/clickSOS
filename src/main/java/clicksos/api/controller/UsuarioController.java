2dc4cea31e21d8938e710c6ba7a284b6991ff4ca
Robson Durães <duraesrobson@gmail.com>
2025-09-10 03:40:41 -0300

    feat: criar controller do Usuario com post para criaUsuario

---
+++ src/main/java/clicksos/api/controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package clicksos.api.controller;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import clicksos.api.dto.usuario.DadosCriarUsuario;
+import clicksos.api.dto.usuario.DadosUsuario;
+import clicksos.api.service.UsuarioService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity criarUsuario(@RequestBody @Valid DadosCriarUsuario dados, UriComponentsBuilder uriBuilder) {
+        var usuario = usuarioService.criaUsuario(dados);
+        var uri = uriBuilder.path("/usuarios/{id}").buildAndExpand(usuario.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosUsuario(usuario));
+    }
+
+}
