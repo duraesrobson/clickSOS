cd7ab0c8d301192ab97a4ece2ce785f2799e1ce4
Robson Durães <duraesrobson@gmail.com>
2025-09-09 23:36:21 -0300

    batman

---
+++ src/test/java/clicksos/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package clicksos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
