a48da0a396a03881cad9bbf34e949c26e2330fbc
Robson Durães <duraesrobson@gmail.com>
2025-09-12 05:25:34 -0300

    criacao do diretorio mobile com o projeto react native + expo com tabs e expo-router

---
+++ mobile/clickSOS/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Stack } from 'expo-router';
+
+export default function Index() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
