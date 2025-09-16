8d1af56c0503ad716cf7c212270c406a63994071
Robson Durães <duraesrobson@gmail.com>
2025-09-16 05:17:36 -0300

    config para usar variaveis do railway e render

--- src/main/java/clicksos/api/ApiApplication.java
+++ src/main/java/clicksos/api/ApiApplication.java
@@ -9,14 +9,20 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("EMAIL", dotenv.get("EMAIL"));
-		System.setProperty("SENHA_APP_EMAIL", dotenv.get("SENHA_APP_EMAIL"));
+		// para usar o render cpm railway
+		if (System.getenv("DB_URL") == null) {
+			try {
+				Dotenv dotenv = Dotenv.load();
+				System.setProperty("DB_URL", dotenv.get("DB_URL"));
+				System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+				System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+				System.setProperty("EMAIL", dotenv.get("EMAIL"));
+				System.setProperty("SENHA_APP_EMAIL", dotenv.get("SENHA_APP_EMAIL"));
+			} catch (Exception e) {
+				System.err.println("Aviso: .env não encontrado. Usando variáveis de ambiente do sistema.");
+			}
+		}
 
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
