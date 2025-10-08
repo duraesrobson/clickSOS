0307c1ef9d69f2d1c9dce943d034a71442426d87
Robson Durães <duraesrobson@gmail.com>
2025-10-08 20:42:16 -0300

    mobile: fix; margem no InfoCard

--- mobile/clickSOS/components/InfoCard.tsx
+++ mobile/clickSOS/components/InfoCard.tsx
@@ -10,7 +10,7 @@ interface InfoCardProps {
 
 const InfoCard: React.FC<InfoCardProps> = ({ title, iconName, iconColor = '#1e6ba5', children }) => {
   return (
-    <View className="bg-white p-4 rounded-xl shadow m-4 w-full">
+    <View className="bg-white p-4 rounded-xl shadow mb-4 mt-4 w-full">
       <View className="flex-row items-center mb-3">
         {iconName && (
           <FontAwesome name={iconName} size={14} color={iconColor} className="mr-2" />
